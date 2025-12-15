cd972f8fe00baeebcc0a11d3e5b8faddba726faa
Jerry <playstore1392004@gmail.com>
2025-12-15 23:21:10 +0530

    Once again

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import gsap from 'gsap';
+// import {useGSAP} from '@gsap/react';
+
+import About from "./components/About";
+import Hero from "./components/Hero";
+
+
+function App() {
+
+  return (
+    <>
+      <Hero />
+      <About />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import '../App.css';
+
+
+function Hero() {
+  
+  useEffect(() => {
+    const loader = document.getElementById("profile");
+    window.addEventListener("load", () => {
+      loader.classList.add('active')
+    })
+  },[]);
+
+
+  return (
+    <section className="hero">
+      <div className="container">
+        <div className="wrapper">
+          <span className="name">Deep Mujpara</span>
+          <span className="profile" id='profile'>
+            <img src="./profile.jpg" alt="" />
+          </span>
+
+          <span className="portfolio">Portfolio</span>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
