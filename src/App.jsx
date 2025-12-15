c2eab81b4de38f476ab4a017d775f25755499804
Jerry <playstore1392004@gmail.com>
2025-12-16 00:01:58 +0530

    Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-// import gsap from 'gsap';
-// import {useGSAP} from '@gsap/react';
-
+import { useEffect,useState } from "react";
 import About from "./components/About";
 import Hero from "./components/Hero";
-
+import './App.css';
 
 function App() {
+const [loading, setLoading] = useState(true)
 
+useEffect(()=>{
+  setLoading(false)
+},[])
   return (
     <>
+      {loading && <div className="loader"></div>}
       <Hero />
       <About />
     </>
