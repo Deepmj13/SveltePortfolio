945589fdced84daeec3fa2f589e94fde6fb9ac30
Jerry <playstore1392004@gmail.com>
2025-12-29 23:14:05 +0530

    Svelte

---
+++ src/components/Hero.svelte
@@ -0,0 +1,12 @@
+<section class="hero">
+  <header>
+    <span class="portfolio"> Portfolio </span>
+    <span class="contact">
+      <a href="#contact">Contact</a>
+    </span>
+  </header>
+  <div class="content">
+    <h1>Deep Mujpara</h1>
+    <h4>Software Developer</h4>
+  </div>
+</section>
